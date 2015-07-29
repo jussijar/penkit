3a1212b9adbe3d7f62c40df90634689d80043636
Jussi Jartamo <jussi.jartamo@gmail.com>
2015-07-29 23:40:02 +0300

    retrieve and store user details into localstorage

--- src/client/penkit.cljs
+++ src/client/penkit.cljs
@@ -2,8 +2,12 @@
   (:use-macros [purnam.core :only [obj arr ! def.n def*n def* do*n]]
                [gyr.core :only [def.module def.filter
                                 def.factory def.controller]]))
+(enable-console-print!)
 (def.module penkit [])
-(def.controller penkit.PenkitController [$scope $interval]
+(defn localStorage->user 
+	[u] (if u (.fromJson js/angular u) (obj :sex "FEMALE")))
+(def.controller penkit.PenkitController [$scope $interval $window]
+	(! $scope.user (localStorage->user (.getItem $window.localStorage :PENKIT!)))
 	(def socket (js/io))
 	(. socket (on "NOSTOT" (fn [msg] (! $scope.teams msg))))
 	($interval (fn [] (! $scope.showDiff (not $scope.showDiff))) 1500)
@@ -12,5 +16,9 @@
 	(! $scope.addLift
 		(fn [form]
 			(if form.$valid
-				(.emit socket "NOSTO!" $scope.user)))))
+				(do (print $scope.user)
+					(.emit socket "NOSTO!" $scope.user)
+					(.setItem $window.localStorage :PENKIT! (.toJson js/angular $scope.user)))
+					
+				))))
 					   
