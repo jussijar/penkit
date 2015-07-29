3339d6ae89875064cde0a87fedb625a1070477e2
Jussi Jartamo <jussi.jartamo@gmail.com>
2015-07-29 23:51:19 +0300

    broadcast update when user submits

--- src/server/server.cljs
+++ src/server/server.cljs
@@ -1,6 +1,6 @@
 (ns penkit.server
   (:require [cljs.nodejs :as node]))
-
+(enable-console-print!)
 (def express (node/require "express"))
 (def app (express))
 (def staticResources (. express (static "resources")))
@@ -11,9 +11,11 @@
 
 (defn -main [& args]
 	(. io (on "connection" (fn [socket] 
-		(.log js/console "Incoming")
-		(.log js/console "User")
-		(. socket (on "NOSTO!" (fn [msg] (.log js/console msg))))
+		(. socket (on "NOSTO!" (fn [msg] 
+			(.emit io "NOSTOT" (clj->js [{:name "OPH"
+				 :totalScore {:kg 1234 :sinclair 435.44 :wilks 546.44}
+				 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} }]))
+			)))
 		(.emit socket "NOSTOT" (clj->js [{:name "OPH"
 					 :totalScore {:kg 1234 :sinclair 435.44 :wilks 546.44}
 					 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} } 
@@ -23,11 +25,10 @@
 					{:name "VISIO"
 					 :totalScore {:kg 1234.44 :sinclair 435.44 :wilks 546}
 					 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} }]))
-		)))
-	
+		(print "It's on!"))))
 	(. app (use staticResources))
 	(. app (use compiledJs))
-	(. http (listen 3000 (fn [] (.log js/console "Upsy-daisy"))))
+	(. http (listen 3000 (fn [] (print "Upsy-daisy"))))
 	)
 
 (set! *main-cli-fn* -main)
