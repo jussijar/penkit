394b05b7d654672d6552d4a36a6ecaed8a8aca61
Jussi Jartamo <jussi.jartamo@gmail.com>
2015-07-31 01:55:05 +0300

    new data format and mongodb insert, update and read aggregation

--- src/client/penkit.cljs
+++ src/client/penkit.cljs
@@ -6,6 +6,21 @@
 (def.module penkit [])
 (defn localStorage->user 
 	[u] (if u (.fromJson js/angular u) (obj :sex "FEMALE")))
+(defn pow [x p] (Math/pow x p))
+(defn log10 [x] (Math/log10 x))
+(defn sinclairM [w l] 
+	(* l (pow 10 (* 0.794358141 (pow (log10 (/ w 174.393)) 2)))))
+(defn sinclairF [w l] 
+	(* l (pow 10 (* 0.897260740 (pow (log10 (/ w 148.026)) 2)))))
+(defn sinclair [w l s] (if (= s "MALE") (sinclairM w l) (sinclairF w l)))
+(defn wilksM [w l] 
+	(* l (/ 500 (+ -216.0475144 (* 16.2606339 w) (* -0.002388645 (pow w 2))
+	(* -0.00113732 (pow w 3)) (* 7.01863E-06 (pow w 4)) (* -1.291E-08 (pow w 5))))))
+(defn wilksF [w l] 
+	(* l (/ 500 (+ 594.31747775582 (* -27.23842536447 w) (* 0.82112226871 (pow w 2))
+	(* -9.30733913E-03 (pow w 3)) (* 4.731582E-05 (pow w 4)) (* -9.054E-08 (pow w 5))))))
+(defn wilks [w l s] (if (= s "MALE") (wilksM w l) (wilksF w l)))
+
 (def.controller penkit.PenkitController [$scope $interval $window]
 	(! $scope.user (localStorage->user (.getItem $window.localStorage :PENKIT!)))
 	(def socket (js/io))
@@ -16,8 +31,13 @@
 	(! $scope.addLift
 		(fn [form]
 			(if form.$valid
-				(do (print $scope.user)
-					(.emit socket "NOSTO!" $scope.user)
+				(do (print $scope.user) ;$scope.user
+					(.emit socket "NOSTO!" (clj->js {
+						:team $scope.user.team 
+						:kg $scope.user.lift 
+						:sc (sinclair $scope.user.weight $scope.user.lift $scope.user.sex)
+						:wk (wilks $scope.user.weight $scope.user.lift $scope.user.sex)
+						}))
 					(.setItem $window.localStorage :PENKIT! (.toJson js/angular $scope.user)))
 					
 				))))
--- src/server/server.cljs
+++ src/server/server.cljs
@@ -8,23 +8,62 @@
 (def http (. (node/require "http") (Server app)))
 (def socketio (node/require "socket.io"))
 (def io (socketio http))
+(def mongo (node/require "mongodb"))
+(def nCollection (atom {}))
+(def nostot (atom (array-map)))
+(defn fetchResults [n f] (.aggregate n (clj->js [
+		{ :$project {	:_id 0 
+						:team 1
+            :kg {:$cond [{:$gt [:$createdAt :null]} 0 :$kg]}
+            :sc {:$cond [{:$gt [:$createdAt :null]} 0 :$sc]}
+            :wk {:$cond [{:$gt [:$createdAt :null]} 0 :$wk]}
+            :dkg {:$cond [{:$gt [:$createdAt :null]} :$kg 0]}
+            :dsc {:$cond [{:$gt [:$createdAt :null]} :$sc 0]}
+            :dwk {:$cond [{:$gt [:$createdAt :null]} :$wk 0]}
+       }}
+	   { :$group { 	:_id :$team
+					:kg {:$sum :$kg} :sc {:$sum :$sc} :wk {:$sum :$wk}
+					:dkg {:$sum :$dkg} :dsc {:$sum :$dsc} :dwk {:$sum :$dwk}
+       }}]) f))
+(def db (.connect mongo.MongoClient "mongodb://nostaja:password@localhost/penkit" (fn [err db] 
+	(fetchResults (reset! nCollection (.collection db "n"))
+		 (fn [err result] (do (print result) (reset! nostot (js->clj result)))))
+)))
 
 (defn -main [& args]
 	(. io (on "connection" (fn [socket] 
-		(. socket (on "NOSTO!" (fn [msg] 
-			(.emit io "NOSTOT" (clj->js [{:name "OPH"
-				 :totalScore {:kg 1234 :sinclair 435.44 :wilks 546.44}
-				 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} }]))
-			)))
-		(.emit socket "NOSTOT" (clj->js [{:name "OPH"
-					 :totalScore {:kg 1234 :sinclair 435.44 :wilks 546.44}
-					 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} } 
-					{:name "LIHA"
-					 :totalScore {:kg 1234.44 :sinclair 435 :wilks 546.44}
-					 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} }
-					{:name "VISIO"
-					 :totalScore {:kg 1234.44 :sinclair 435.44 :wilks 546}
-					 :difference {:kg 345.55 :sinclair 34.55 :wilks 564} }]))
+		(. socket (on "NOSTO!" (fn [msgjson] 
+			(let [msg (js->clj msgjson)]
+			(do (print msg)
+				(let [out { 
+					:$set {:team (get msg "team")}
+					:$inc {
+					:kg (get msg "kg")
+					:sc (get msg "sc")
+					:wk (get msg "wk")
+				}}]
+				(.update @nCollection 
+					(clj->js {:$and [
+						{:createdAt {:$exists false}}
+						{:team (get msg "team")}
+					]})
+					(clj->js out)
+					(clj->js {:upsert true})
+					(fn [err res] (print err)))
+				(.insert @nCollection 
+					(clj->js {
+						:createdAt (new js/Date)
+						:team (get msg "team")
+						:kg (get msg "kg")
+						:sc (get msg "sc")
+						:wk (get msg "wk")
+					})
+					(fn [err res] (fetchResults @nCollection (fn [e result] 
+						(.emit io "NOSTOT" (clj->js (reset! nostot (js->clj result))))
+					)))
+				)
+				))))))
+		(.emit socket "NOSTOT" (clj->js @nostot))
 		(print "It's on!"))))
 	(. app (use staticResources))
 	(. app (use compiledJs))
